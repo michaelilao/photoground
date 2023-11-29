d613be16b9ed94ba5170cf935c7310a14efeeaee
Michael Ilao <michael@immigrate.ai>
2023-11-29 11:40:50 -0500

    file structure

--- migration/init.js
+++
@@ -1,6 +0,0 @@
-exports.createUsersTable = `CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name text,\ 
-    email text UNIQUE,\
-    password text,\
-    CONSTRAINT email_unique UNIQUE (email))`;
-
-exports.insertAdminRecord = `INSERT INTO users (name, email, password) VALUES (?,?,?)`
